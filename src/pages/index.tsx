577e34b037f0e6bcb0cf10250b2e11334fa425c1
hukuryo <ryohei.1998.05.09@gmail.com>
2023-04-09 17:44:41 +0900

    ⚗️  不要なイベントを、実験的に書いてみました。いつか消す

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,29 @@ import styles from '@/src/styles/Home.module.css';
 import { Links } from '@/src/components/Links/Links';
 import { Title } from '@/src/components/Title/Title';
 import { Header } from '@/src/components/Header/Header';
+import { useCallback } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
+
 export default function Home() {
+  const foo = 1;
+  
+  const handleClick = useCallback ((e:any) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(foo);
+  }, []);
+
   return (
     <>
       <Head>
         <title>Index Page</title>
       </Head>
-
       <Header />
+      <a href='/about' onClick={handleClick}>
+        ボタン
+      </a>
       <main className={styles.main}>
         <div className={styles.description}>
         <Headline title="index page"/>
