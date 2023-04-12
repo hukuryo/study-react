e64b88591a8936bf2c97d07fdc58676965e7d269
hukuryo <ryohei.1998.05.09@gmail.com>
2023-04-12 21:14:57 +0900

    ♻️ useStateで文字列と真偽値を書いた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,12 +12,18 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   const [count, setFoo] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback (() => {
     if(count < 10){
       setFoo((count) => count + 1);
     }
   }, [count]);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow)
+  }, []);
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -32,10 +38,15 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
-      <button onClick={handleClick}>
-        ボタン
-      </button>
+      {isShow ?  <h1>{count}</h1> : null}
+      <button onClick={handleClick}>ボタン</button>
+
+      <button onClick={handleDisplay}>
+      {isShow ?  "非表示" : "表示"}</button>
+
+      <input type="text" value={text} onChange={e => {
+        setText(e.target.value)
+      }} />
       <main className={styles.main}>
         <div className={styles.description}>
         <Headline title="index page"/>
